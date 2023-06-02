0b88c85c80674359e802be87a23d7013c9bece96
EanJang <sooyean.jang@gmail.com>
2023-06-02 14:43:32 +0900

    Implement Logout feature w/ Keycloak

---
+++ Auth/keycloak/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="gnb">
+    <span class="mdi mdi-account-outline btn"></span>
+    <!-- tmp logout button -->
+    <button class="logoutBtn" @click="logout">LOG OUT</button>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { inject } from "vue";
+const logout = inject("logout") as () => void;
+</script>
+
+<style lang="scss">
+// tmp logout button
+.logoutBtn {
+  border: none;
+  background: none;
+  cursor: pointer;
+}
+</style>
