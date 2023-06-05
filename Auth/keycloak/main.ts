5871596b2326a08593531e66d77edec454e6c473
EanJang <sooyean.jang@gmail.com>
2023-06-05 19:28:22 +0900

    Update main.ts with if statement for VUE_APP_KEYCLOAK variable

--- Auth/keycloak/main.ts
+++ Auth/keycloak/main.ts
@@ -1,38 +1,39 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "primevue/resources/primevue.min.css";
+import "@/styles/main.scss";
 
 import Keycloak from "keycloak-js";
 import type { KeycloakOnLoad } from "keycloak-js";
 
-import PrimeVue from "primevue/config";
-
-const initOptions = {
-  // url: process.env.VUE_APP_KEYCLOAK_URL,
-  url: "https://keycloak-admin-console",
-  realm: "portal",
-  clientId: "portal",
-  onLoad: "login-required" as KeycloakOnLoad,
-};
-
-const keycloak = new Keycloak(initOptions);
-keycloak
-  .init({ onLoad: initOptions.onLoad })
-  .then((auth: any) => {
-    console.log("auth status", auth);
-    if (!auth) {
-      window.location.reload();
-    } else {
-      const app = createApp(App);
-      app.provide("logout", logout);
-      app.use(router).use(PrimeVue).mount("#app");
-    }
-  })
-  .catch((error: any) => {
-    console.log("ERROR: ", error);
-  });
-
-function logout() {
-  keycloak.logout({ redirectUri: process.env.BASE_URL });
+console.log(process.env.VUE_APP_KEYCLOAK);
+if (process.env.VUE_APP_KEYCLOAK) {
+  const initOptions = {
+    url: process.env.VUE_APP_KEYCLOAK_URL,
+    realm: "realmNAME",
+    clientId: "clientID",
+    onLoad: "login-required" as KeycloakOnLoad,
+  };
+  const keycloak = new Keycloak(initOptions);
+  const logout = () => {
+    keycloak.logout({ redirectUri: process.env.BASE_URL });
+  };
+  keycloak
+    .init({ onLoad: initOptions.onLoad })
+    .then((auth: any) => {
+      if (!auth) {
+        window.location.reload();
+      } else {
+        const app = createApp(App);
+        app.provide("logout", logout);
+        app.use(router).mount("#app");
+      }
+    })
+    .catch((error: any) => {
+      console.log("ERROR: ", error);
+    });
+} else {
+  const app = createApp(App);
+  app.use(router);
+  app.mount("#app");
 }
