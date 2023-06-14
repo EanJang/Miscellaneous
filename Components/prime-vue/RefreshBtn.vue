716108a0d3906784cbab09b52738607da27f4ab2
EanJang <sooyean.jang@gmail.com>
2023-06-14 16:37:16 +0900

    button component with prime-vue and vue3
    for refresh feature in time picker

---
+++ Components/prime-vue/RefreshBtn.vue
@@ -0,0 +1,9 @@
+<template>
+  <BaseButton label="">HELLO</BaseButton>
+</template>
+
+<script setup lang="ts">
+import BaseButton from "./base/BaseButton.vue";
+</script>
+
+<style lang="scss" scoped></style>
